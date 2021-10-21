bd0416db57b15e1afb908905fdbd3d6fc9c2c24b
Jean Igor de Queiroz Pantoja <39068801+srpantoja@users.noreply.github.com>
2021-10-21 12:23:09 -0300

    Update OutputValidator.js

--- src/utils/OutputValidator.js
+++ src/utils/OutputValidator.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises
 
 async function validateOutput(output, expectedOutput) {
     
-    output = output.split('\r\n')
+    //output = output.split('\r\n')
     console.log(output[0])
     return output[0] == expectedOutput
 }
