809af6a64bf6a704a4b482928e07eb9dc6022e07
Jean Pantoja <39068801+srpantoja@users.noreply.github.com>
2022-01-16 13:49:36 -0300

    #add populate houseid

--- src/controllers/Problem.js
+++ src/controllers/Problem.js
@@ -1,35 +1,35 @@
 const ProblemModel = require('../models/Problem')
 const view = require('../views/Problem')
- 
- module.exports.createProblem = (req, res) => {
+
+module.exports.createProblem = (req, res) => {
     let problem = req.body
     let promise = ProblemModel.create(problem)
     console.log(problem)
-    promise.then((problem)=>{
+    promise.then((problem) => {
         res.status(201).json(view.render(problem))
-    }).catch((error)=>{
-        res.status(400).json({message: error})
+    }).catch((error) => {
+        res.status(400).json({ message: error })
     })
 }
 
 
 module.exports.listProblem = (req, res) => {
-    let promise = ProblemModel.find().populate("courses").populate("modules").exec()
-    promise.then((problem)=>{
+    let promise = ProblemModel.find().populate("id_house").populate("courses").populate("modules").exec()
+    promise.then((problem) => {
         res.status(200).json(view.renderMany(problem))
-    }).catch((error)=>{
-        res.status(400).json({message: "error message", error: error})
+    }).catch((error) => {
+        res.status(400).json({ message: "error message", error: error })
     })
 }
 
 module.exports.findProblem = (req, res) => {
     let id = req.params.id
     let promise = ProblemModel.findById(id).exec()
-    promise.then((problem)=>{
+    promise.then((problem) => {
         res.status(200).json(view.render(problem))
-    }).catch((error)=>{
-            res.status(404).json({message: "problem not found", error: error})
-        }
+    }).catch((error) => {
+        res.status(404).json({ message: "problem not found", error: error })
+    }
     )
 }
 
@@ -37,19 +37,24 @@ module.exports.findProblem = (req, res) => {
 module.exports.deleteProblem = (req, res) => {
     let id = req.params.id
     let promise = ProblemModel.findByIdAndDelete(id).exec()
-    promise.then((problem)=>{
+    promise.then((problem) => {
         res.status(200).json(view.render(problem))
-    }).catch((error)=>{
-            res.status(400).json({message: "problem not found", error: error})
-        }
+    }).catch((error) => {
+        res.status(400).json({ message: "problem not found", error: error })
+    }
     )
 }
 
-module.exports.findProblemByCourseAndModule = (req,res) => {
-    let promise = ProblemModel.find({'$and':[{courses: {'$in': [req.params.courseId]}}, {modules:{'$in':[req.params.moduleId]}} ]}).populate("courses").populate("modules").exec()
+module.exports.findProblemByCourseAndModule = (req, res) => {
+    let promise = ProblemModel.find(
+        { '$and': [{ courses: { '$in': [req.params.courseId] } }, { modules: { '$in': [req.params.moduleId] } }] })
+        .populate("id_house")
+        .populate("courses")
+        .populate("modules")
+        .exec()
     promise.then((problem) => {
         res.status(200).json(view.renderMany(problem))
     }).catch((error) => {
-        res.status(400).json({message:"error", error: error})
+        res.status(400).json({ message: "error", error: error })
     })
 }
